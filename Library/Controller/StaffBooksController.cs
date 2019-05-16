1badcb27ed583ee4e447ebe269b2a1b56ac89e43
Tessa Sullivan and Zach Weintraub <zach@>
2019-05-16 15:42:07 -0700

    rough draft of StaffBooks Create route and New form

--- Library/Controller/StaffBooksController.cs
+++ Library/Controller/StaffBooksController.cs
@@ -24,22 +24,49 @@ namespace Library.Controllers
     public ActionResult Create(string title, int year, string first1, string last1, string first2, string last2, string first3, string last3)
     {
       // Check if combo of book & year exists, if so, error
-      // Otherwise add book and save it
-      // Check if author exists, if not, then create them, if so, then get the author id
-      // If author field is blank, skip it
-
-      // Run book.SetBookAuthor for each author
-      // Run book.SetStock
-
-      // Show book
 
       // Otherwise add book and save it
       Book newBook = new Book(title, year);
       newBook.Save();
 
       // Check if author exists, if not, then create them, if so, then get the author id
-      Author author1 = new Author(first1, last1);
-      
+      // If author field is blank, skip it
+      List<Author> authors = new List<Author> {};
+
+      if (first1 != "" || last1 != "")
+      {
+        Author author1 = new Author(first1, last1);
+        if (!author1.DoesAuthorExist())
+        {
+          author1.Save();
+        }
+        authors.Add(author1);
+      }
+
+      if (first2 != "" || last2 != "")
+      {
+        Author author2 = new Author(first2, last2);
+        if (!author2.DoesAuthorExist())
+        {
+          author2.Save();
+        }
+        authors.Add(author2);
+      }
+      if (first3 != "" || last3 != "")
+      {
+        Author author3 = new Author(first3, last3);
+        if (!author3.DoesAuthorExist())
+        {
+          author3.Save();
+        }
+        authors.Add(author3);
+      }
+      // Run book.SetBookAuthor for each author
+      // Run book.SetStock
+
+      // Show book
+
+
 
 
 
